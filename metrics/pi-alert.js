2ef2c506d0ae0e30d7ab6c25b99cb93a4f41e719
Andrew Lloyd <andrew85.lloyd@gmail.com>
2020-04-02 22:00:49 -0700

    Add add disk alert metrics collection from pi and handling API

---
+++ metrics/pi-alert.js
@@ -0,0 +1,40 @@
+'use latest';
+const express = require('express');
+const { fromExpress } = require('webtask-tools');
+const app = express();
+app.use(express.json());
+const Twilio = require('twilio');
+const ALERT_USAGE = 80;
+
+function sendText(secrets, message) {
+  const client = new Twilio(secrets.ACCOUNT_SID, secrets.AUTH_TOKEN);
+  client.messages.create({
+    body: message,
+    to: secrets.TO,
+    from: secrets.FROM
+  })
+  .catch(e => {
+    console.log(e);
+  })
+}
+
+app.post('/api/metrics', (req, res) => {
+  const secrets = req.webtaskContext.secrets;
+  if (req.headers["x-api-key"] === secrets.API_TOKEN) {
+    const keys = Object.keys(req.body.metrics)
+    req.body.metrics.forEach((metric) => {
+      const key = Object.keys(metric);
+      if (metric[key] >= ALERT_USAGE) {
+        sendText(secrets, `Pi volume ${key} alert, disk usage ${metric[key]}% is above ${ALERT_USAGE}%`)
+      }
+    });
+
+    res.set('Content-Type', 'application/json');
+    res.status(200).json({result: 'success'});
+  } else {
+    console.log("Unauthorized request")
+    res.status(400).json({err: 'NOT AUTHORIZED'});
+  }
+});
+
+module.exports = fromExpress(app);
